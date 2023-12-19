8bfebf9c946351284fc0077300eeae3441dd8840
Yurii <gapirov21@gmail.com>
2023-12-19 23:41:01 +0200

    done baseTypes

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,10 @@
-let some:unknown;
-some = 'Text';
+let some: unknown;
+some = "Text";
 let str: string;
-str = some;
+
+if (typeof some === "string") {
+  //перевірили чи соме стрінга,
+  str = some; // передали після перевірки стрінгу змінній лет
+}
 
 export {};
--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,22 +2,34 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
+type Pages = {
+  title: string;
+  likes: number;
+  accounts: string[]; //cтрінга масив
+  status: "open" | "close"; //union type
+  details?: {
+    //опціольна властивість (не обовязкова)
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
 const page1 = {
-  title: 'The awesome page',
+  title: "The awesome page",
   likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
   details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
 
 const page2 = {
-  title: 'Python or Js',
+  title: "Python or Js",
   likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+  accounts: ["Alex"],
+  status: "close",
+};
 
 export {};
